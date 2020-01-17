8db9f886087c2ce5b83be038dd88ad0e016bc38f
李宵逸 <13691419520@163.com>
2020-01-16 21:07:29 -0800

    Convert data to matrix type (failed)

--- tmp.R
+++ tmp.R
@@ -1,3 +1,11 @@
 source("TDAmisc.R")
+
+# Read data
 mnist <- read.csv('mnist.csv')
+
+# Make data into `images` and `labels` vector
+images <- matrix(unlist(mnist[, 1:784]), ncol=784, byrow=TRUE)
+labels <- as.matrix(mnist[785])
+# TODO: found this in https://stackoverflow.com/a/13224720, but out of memory on
+#  my (Eric Li) laptop
 
