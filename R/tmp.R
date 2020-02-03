22b8a1c8ffa3a994251b72dcdb1979efac9785a3
李宵逸 <13691419520@163.com>
2020-02-02 22:59:20 -0800

    Use as.matrix to solve oom problem

--- R/tmp.R
+++ R/tmp.R
@@ -1,11 +1,19 @@
-source("TDAmisc.R")
+source("TDAsweep.R")
 
 # Read data
-mnist <- read.csv('mnist.csv')
+mnist <- read.csv('../mnist.csv')
 
-# Make data into `images` and `labels` vector
-images <- matrix(unlist(mnist[, 1:784]), ncol=784, byrow=TRUE)
+# Make data into `images` matrix and `labels` vector
+# images <- matrix(unlist(mnist[, 1:784]), ncol=784, byrow=TRUE)
+images <- as.matrix(mnist[, 1:784])
 labels <- as.matrix(mnist[785])
-# TODO: found this in https://stackoverflow.com/a/13224720, but out of memory on
-#  my (Eric Li) laptop
+# found this in https://stackoverflow.com/a/13224720
+
+# Plot data
+for (i in 1:70000) {
+	filled.contour(matrix(images[i, ], 28, byrow=T))
+}
+
+# Invoke TDAsweepOneImg?
+TDAsweepOneImg(matrix(images[1,], ncol=28), 28, 28)
 
