0d2b77d7f03b5b062864781bbe2aaa356c156f2a
Yu-Shih Chen <yu-shih@campus-118-086.ucdavis.edu>
2020-03-03 10:10:08 -0800

    Changes to tda_wrapper. Added rgb handling and intervalWidth parameter. Removed subsetting feature

--- R/tda_wrapper.R
+++ R/tda_wrapper.R
@@ -2,64 +2,60 @@ library(regtools)
 source("./TDAsweep.R")
 source("./TDAprep.R")
 
-# Questions:
-# 1. is the user going to put in raw images, or are they going to do some processing to make it like csv MNIST?
-# 2. Do we need subset parameter? Or is it better to let the user just subset themselves?
+# Updates:
+# 1. (tested) rbg handling done, assuming intake vector (24x24x3 will be 1x1728 vector)
+# 2. (tested) removed subset feature
+# 3. (tested) added intervalWidth parameter
+# 4. Found that "Bug" was because the picture was rotated. The code is correct
 
-# Todo:
-# 1. finish RGB handling
-# 2. make the code read from the user input images
-# 3. test out and de-bug
 
-# This function takes in images matrix and labels vector. 
-tda_wrapper_func <- function(images, labels, nr, nc, subset = NULL, thresh = 0){
+# Parameters
+# images: dataframe or matrix of sets of vectors of image pixels
+# labels: dataframe or vector of sets of labels
+# nr: Integer. Number of row of image
+# nc: Integer. Number of col of image
+# rgb: Bool. If images are rgb or grey scale
+# thresh: Integer. Threshold for pixels
+# intervalWidth: Integer. Interval Width for each sweep
+
+tda_wrapper_func <- function(images, labels, nr, nc , rgb=TRUE, thresh = 0, intervalWidth=1){
   
-  ############################## Semi-Pseudocode for handling RGB images ####################################
-  # if(RGB == TRUE){  # run sweep three times
-  #   tda_result <- NULL  # list to append later for R,G,B sweeps
-  #   images_R = images[,,1]  # R layer
-  #   images_G = images[,,2]  # G layer
-  #   images_B = images[,,3]  # B layer
-  #   for(images in c(images_R, images_G, images_B)){ 
-  #     tda_result <- c(tda_result, tda_sweep(images=images, nr=nr, nc=nc, subset=subset, thresh=thresh))
-  #   }
-  #   return(tda_result)
-  # }else{  # run sweep one time
-  #   tda_result <- tda_sweep(images=images, nr=nr, nc=nc, subset=subset, thresh=thresh)
-  #   return(tda_result)
-  # }
-  
-  ############################## this part is processing the data using prepImgSet ##############################
-  prepImgs <- prepImgSet(images, nr=nr, labels=labels, thresh=thresh)  
-  
-  ############################## this part is performing TDASweep ##############################
-  sweep_result <- TDAsweepImgSet(prepImgs, nr=nr, nc=nc)
-  return (sweep_result)
-  
-  # tda_sweep <- function(images=images, nr=nr, nc=nc, subset=subset, thresh=thresh){  # basic pipeline function for tda-sweep in one set of images
+  tda_sweep <- function(images, labels, nr, nc, thresh, intervalWidth){  # basic pipeline function for tda-sweep in one set of images
     
-  #   ############################## this part is checking if the user wants subset and preparing features and labels data ##############################
-  #   #if(is.null(subset)){  # if the user wants to subset
-  #     img_pixels <- mnist[, -785]
-  #     labels <- mnist[, 785]
-  #   #}#else{  # if the user wants every row
-  #    # set.seed(999) 
-  #     #n <- sample(1:nrow(mnist), subset)  
-  #     #img_pixels <- mnist[n, -785]
-  #     #labels <- mnist[n, 785]
-  #   #}
-  #   return(sweep_result)
-  # }
+    ############################## Data preparation ##############################
+    img_pixels <- images
+    labels <- labels
+    
+    ############################## Processing data ##############################
+    prepImgs <- prepImgSet(img_pixels, nr=nr, labels=labels, thresh=thresh)
+    
+    ############################## TDASweep ##############################
+    sweep_result <- TDAsweepImgSet(prepImgs, nr=nr, nc=nc, intervalWidth=intervalWidth)
+    
+    return(sweep_result)
+  }
+  
+  ############################## Reading the csv data from the user ##############################
+  images_df <- as.data.frame(images)  
+  labels <- labels
+  
+  ############################## Handling RGB images ####################################
+  if(rgb == TRUE){  # run sweep three times
+    tda_result <- NULL  # list to append later for R,G,B sweeps
+    one_layer_size <- nr*nc
+    images_R = images_df[, 1:one_layer_size]  # R layer
+    images_G = images_df[, (one_layer_size+1):(2*one_layer_size)]  # G layer
+    images_B = images_df[, (2*one_layer_size+1):(3*one_layer_size)]  # B layer
+    # Run TDA Sweep for R, G, and B layers
+    tda_result_R <- tda_sweep(images=images_R, labels=labels, nr=nr, nc=nc, thresh=thresh, intervalWidth=intervalWidth)
+    tda_result_G <- tda_sweep(images=images_G, labels=labels, nr=nr, nc=nc, thresh=thresh, intervalWidth=intervalWidth)
+    tda_result_B <- tda_sweep(images=images_B, labels=labels, nr=nr, nc=nc, thresh=thresh, intervalWidth=intervalWidth)
+    new_dataset <- cbind(tda_result_R, tda_result_G, tda_result_B, labels)
+    return(new_dataset)
+  }else{  # when rgb=FALSE
+    tda_result <- tda_sweep(images=images_df, labels=labels, nr=nr, nc=nc, thresh=thresh, intervalWidth=intervalWidth)
+    new_dataset <- cbind(tda_result, labels)
+    return(new_dataset)
+  }
 }
 
-# Samples a random image from imgset.
-test_one_img <- function(imgset, labels, nr, nc, thresh) {
-  idx <- sample(1:nr, 1)
-  img2d <- imgTo2D(imgset[idx,], nr)
-  label <- labels[idx]
-  print(label)
-  print(matrix(img2d[,3], nr, nc))
-  res <- prepOneImage(img2d, thresh)
-  res <- TDAsweepOneImg(res, nr, nc)
-  return (res)
-}
