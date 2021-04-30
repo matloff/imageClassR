eedc961cc76af5c56b6ee865d95431cd1e9100d9
matloff <matloff@cs.ucdavis.edu>
2021-04-29 23:07:19 -0700

    next, predict.drmlTDA

--- R/drmlFitPred.R
+++ R/drmlFitPred.R
@@ -11,51 +11,49 @@
 #       the choices in the qe*() series
 #    mlFtnArgs:  algorithm-specifc arguments, R list of named elements
 
-#####  drmlTDA <- function(imgs,labels,nr,nc,rgb=TRUE,
-#####     thresholds=c(50,100,150),intervalWidth=2,cls=NULL,rcOnly=TRUE,
-#####     qeFtn,opts=NULL,holdout=floor(min(1000,0.1*nrow(images)))
-#####  {
-#####     res <- list()  # ultimate return value
-#####  
-#####     # must deal with constant columns, typically all-0, as many ML algs try to
-#####     # scale the data and will balk; remove such columns, and make a note
-#####     # so the same can be done during later prediction
-#####     ccs <- constCols(tdaout)
-#####     tdaout <- tdaout[,-ccs]
-#####     res$deletedCols <- ccs
-#####     warning('constant columns have been removed from the input data')
-#####  
-#####     tdaout <- TDAsweep(imgs=imgs,labels=labels,nr=nr,nc=nc,rgb=rgb,
-#####        thresholds=thresholds,intervalWidth=intervalWidth,cls=cls,rcOnly=rcOnly)
-#####  
-#####     # construct the qe*() series call
-#####     tdaout$labels <- as.factor(labels)
-#####     mlcmd <- paste0(qeFtn,'(tdaout,"labels"')
-#####     if (is.null(mlFtnArgs)) mlcmd <- paste0(mlcmd,')')  # more args?
-#####     else {
-#####        nms <- names(mlFtnArgs)
-#####        for (i in 1:length(nms)) {
-#####           mlcmd <- paste0(mlcmd,',')
-#####           argval <- mlFtnArgs[[nms[i]]]
-#####           arg <- paste0(nms[i],'=',argval)
-#####           if (i == length(nms)) mlcmd <- paste0(mlcmd,arg,')')
-#####        }
-#####     }
-##### `print(mlcmd)
-##### `
-##### `   # exeecute the command and set result for return value
-##### `   mlout <- eval(parse(text=mlcmd))
-##### `   mlout$nr <- nr
-##### `   mlout$nc <- nc
-##### `   mlout$rgb <- rgb
-##### `   mlout$thresholds <- thresholds
-##### `   mlout$intervalWidth <- intervalWidth
-##### `   mlout$rcOnly <- rcOnly
-##### `   mlout$constCols <- ccs
-##### `   mlout$classNames <- levels(tdaout$labels)
-##### `   class(mlout) <- c('tdaFit',class(mlout))
-##### `   mlout
-##### `}
+drmlTDA <- function(imgs,labels,nr,nc,rgb=TRUE,
+   thresh=c(50,100,150),intervalWidth=2,cls=NULL,rcOnly=TRUE,
+   qeFtn,opts=NULL,holdout=floor(min(1000,0.1*nrow(images))))
+{
+
+   tdaout <- TDAsweepImgSet(imgs=imgs,labels=labels,nr=nr,nc=nc,
+      thresh=thresh,intervalWidth=intervalWidth,rcOnly=rcOnly)
+
+   # must deal with constant columns, typically all-0, as many ML algs try to
+   # scale the data and will balk; remove such columns, and make a note
+   # so the same can be done during later prediction
+   ccs <- constCols(tdaout)
+   if (length(ccs) > 0) {
+      tdaout <- tdaout[,-ccs]
+      warning('constant columns have been removed from the input data')
+   }  
+
+   # construct the qe*() series call
+   mlcmd <- paste0(qeFtn,'(tdaout,"labels"')
+   if (is.null(opts)) mlcmd <- paste0(mlcmd,')')  # more args?
+   else {
+      nms <- names(opts)
+      for (i in 1:length(nms)) {
+         mlcmd <- paste0(mlcmd,',')
+         argval <- opts[[nms[i]]]
+         arg <- paste0(nms[i],'=',argval)
+         if (i == length(nms)) mlcmd <- paste0(mlcmd,arg,')')
+      }
+   }
+
+   # exeecute the command and set result for return value
+   mlout <- eval(parse(text=mlcmd))
+   mlout$nr <- nr
+   mlout$nc <- nc
+   mlout$rgb <- rgb
+   mlout$thresh <- thresh
+   mlout$intervalWidth <- intervalWidth
+   mlout$rcOnly <- rcOnly
+   mlout$constCols <- ccs
+   mlout$classNames <- levels(tdaout$labels)
+   class(mlout) <- c('drmlTDA',class(mlout))
+   mlout
+}
 
 predict.tdaFit <- function(object,newImages) 
 {
