e630304a3a6bf7ee6f513792b1d45b256cfa549f
Norman Matloff <normanmatloff@Normans-MacBook.local>
2021-04-29 20:06:05 -0700

    in progress

--- R/drmlFitPred.R
+++ R/drmlFitPred.R
@@ -1,10 +1,7 @@
 
 # high-level functions to provide a "turnkey" environment for image
-# analysts
-
-# tdaFit() inputs images data matrix, returns an object ready for use in
-# prediction; latter is done by generic predict(), again inputting an
-# image data matrix
+# analysts; input images matrix or data frame and the associated labels,
+# output an object to be used in prediction
 
 # uses the qe*() series from regtools
 
@@ -14,51 +11,51 @@
 #       the choices in the qe*() series
 #    mlFtnArgs:  algorithm-specifc arguments, R list of named elements
 
-tdaFit <- function(images,labels,nr,nc,rgb=TRUE,
-   thresholds=0,intervalWidth=1,cls=NULL,rcOnly=FALSE,
-   qeFtn,mlFtnArgs=NULL)
-{
-   if (inherits(images,'matrix')) images <- as.data.frame(images)
-
-   tdaout <- TDAsweep(images=images,labels=labels,nr=nr,nc=nc,rgb=rgb,
-      thresholds=thresholds,intervalWidth=intervalWidth,cls=cls,rcOnly=rcOnly,
-      prep=FALSE)
-   tdaout <- as.data.frame(tdaout$tda_df)  # df of the new features
-
-   # must deal with constant columns, typically all-0, as many ML algs try to
-   # scale the data and wil balk; remove such columns, and make a note
-   # so the same can be done during later prediction
-   ccs <- constCols(tdaout)
-   tdaout <- tdaout[,-ccs]
-
-   # construct the qe*() series call
-   tdaout$labels <- as.factor(labels)
-   mlcmd <- paste0(qeFtn,'(tdaout,"labels"')
-   if (is.null(mlFtnArgs)) mlcmd <- paste0(mlcmd,')')  # more args?
-   else {
-      nms <- names(mlFtnArgs)
-      for (i in 1:length(nms)) {
-         mlcmd <- paste0(mlcmd,',')
-         argval <- mlFtnArgs[[nms[i]]]
-         arg <- paste0(nms[i],'=',argval)
-         if (i == length(nms)) mlcmd <- paste0(mlcmd,arg,')')
-      }
-   }
-print(mlcmd)
-
-   # exeecute the command and set result for return value
-   mlout <- eval(parse(text=mlcmd))
-   mlout$nr <- nr
-   mlout$nc <- nc
-   mlout$rgb <- rgb
-   mlout$thresholds <- thresholds
-   mlout$intervalWidth <- intervalWidth
-   mlout$rcOnly <- rcOnly
-   mlout$constCols <- ccs
-   mlout$classNames <- levels(tdaout$labels)
-   class(mlout) <- c('tdaFit',class(mlout))
-   mlout
-}
+#####  drmlTDA <- function(imgs,labels,nr,nc,rgb=TRUE,
+#####     thresholds=c(50,100,150),intervalWidth=2,cls=NULL,rcOnly=TRUE,
+#####     qeFtn,opts=NULL,holdout=floor(min(1000,0.1*nrow(images)))
+#####  {
+#####     res <- list()  # ultimate return value
+#####  
+#####     # must deal with constant columns, typically all-0, as many ML algs try to
+#####     # scale the data and will balk; remove such columns, and make a note
+#####     # so the same can be done during later prediction
+#####     ccs <- constCols(tdaout)
+#####     tdaout <- tdaout[,-ccs]
+#####     res$deletedCols <- ccs
+#####     warning('constant columns have been removed from the input data')
+#####  
+#####     tdaout <- TDAsweep(imgs=imgs,labels=labels,nr=nr,nc=nc,rgb=rgb,
+#####        thresholds=thresholds,intervalWidth=intervalWidth,cls=cls,rcOnly=rcOnly)
+#####  
+#####     # construct the qe*() series call
+#####     tdaout$labels <- as.factor(labels)
+#####     mlcmd <- paste0(qeFtn,'(tdaout,"labels"')
+#####     if (is.null(mlFtnArgs)) mlcmd <- paste0(mlcmd,')')  # more args?
+#####     else {
+#####        nms <- names(mlFtnArgs)
+#####        for (i in 1:length(nms)) {
+#####           mlcmd <- paste0(mlcmd,',')
+#####           argval <- mlFtnArgs[[nms[i]]]
+#####           arg <- paste0(nms[i],'=',argval)
+#####           if (i == length(nms)) mlcmd <- paste0(mlcmd,arg,')')
+#####        }
+#####     }
+##### `print(mlcmd)
+##### `
+##### `   # exeecute the command and set result for return value
+##### `   mlout <- eval(parse(text=mlcmd))
+##### `   mlout$nr <- nr
+##### `   mlout$nc <- nc
+##### `   mlout$rgb <- rgb
+##### `   mlout$thresholds <- thresholds
+##### `   mlout$intervalWidth <- intervalWidth
+##### `   mlout$rcOnly <- rcOnly
+##### `   mlout$constCols <- ccs
+##### `   mlout$classNames <- levels(tdaout$labels)
+##### `   class(mlout) <- c('tdaFit',class(mlout))
+##### `   mlout
+##### `}
 
 predict.tdaFit <- function(object,newImages) 
 {
